7e01a73330d6c7f86aed049581cb2b518a66297b
aisteron <timotheus@list.ru>
2020-06-30 13:36:47 +0300

    w30.06

--- src/js/config.js
+++ src/js/config.js
@@ -1,16 +1,16 @@
 // локальный сервер
 
-export const CONFIG = {
+/*export const CONFIG = {
 	php_path: 'http://api.local/',
 	fancy_path: ''
 }
-
+*/
 
 
 // боевой сервер
 
-/*
+
 export const CONFIG = {
 	php_path: '/php/callback.php',
 	fancy_path: '/assets'
-}*/
+}
